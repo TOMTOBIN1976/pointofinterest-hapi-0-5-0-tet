73027b8a6e9b0361acbbbbf7eec5cdd6d99ab925
Tom Tobin <tomtobin0@gmail.com>
2022-06-04 02:36:21 +0100

    Final version of Hapi Allication.

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,87 @@
+export const seedData = {
+  users: {
+    _model: "User",
+    homer: {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret",
+    },
+    marge: {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret",
+    },
+    bart: {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret",
+    },
+  },
+  candidates: {
+    _model: "Candidate",
+    lisa: {
+      firstName: "Lisa",
+      lastName: "Simpson",
+      office: "President",
+    },
+    donald: {
+      firstName: "Donald",
+      lastName: "Simpson",
+      office: "President",
+    },
+  },
+  donations: {
+    _model: "Donation",
+    one: {
+      amount: 40,
+      method: "paypal",
+      lat: "52.160858",
+      lng: "-7.152420",
+      donor: "->users.bart",
+      candidate: "->candidates.lisa",
+    },
+    two: {
+      amount: 90,
+      method: "direct",
+      lat: "52.149220",
+      lng: "-6.994620",
+      donor: "->users.marge",
+      candidate: "->candidates.lisa",
+    },
+    three: {
+      amount: 430,
+      method: "paypal",
+      lat: "52.161290",
+      lng: "-7.231540",
+      donor: "->users.homer",
+      candidate: "->candidates.donald",
+    },
+    four: {
+      amount: "Cahir Castle",
+      method: "cash",
+      lat: "52.374500",
+      lng: "-7.927100",
+      donor: "->users.homer",
+      candidate: "->candidates.donald",
+    },
+    five: {
+      amount: "Rock of Cashel",
+      method: "cash",
+      lat: "52.520100",
+      lng: "-7.890500",
+      donor: "->users.homer",
+      candidate: "->candidates.donald",
+    },
+    six: {
+      amount: "Holy Cross Abbey",
+      method: "cash",
+      lat: "52.381379",
+      lng: "-7.515999",
+      donor: "->users.homer",
+      candidate: "->candidates.donald",
+    },
+  },
+};
---
+++ test/fixtures.js
@@ -0,0 +1,66 @@
+export const serviceUrl = "http://localhost:4000";
+
+export const maggie = {
+  firstName: "Maggie",
+  lastName: "Simpson",
+  email: "maggie@simpson.com",
+  password: "secret",
+};
+
+export const maggieCredentials = {
+  email: "maggie@simpson.com",
+  password: "secret",
+};
+
+export const testUsers = [
+  {
+    firstName: "Homer",
+    lastName: "Simpson",
+    email: "homer@simpson.com",
+    password: "secret",
+  },
+  {
+    firstName: "Marge",
+    lastName: "Simpson",
+    email: "marge@simpson.com",
+    password: "secret",
+  },
+  {
+    firstName: "Bart",
+    lastName: "Simpson",
+    email: "bart@simpson.com",
+    password: "secret",
+  },
+];
+
+export const testCandidates = [
+  {
+    firstName: "Lisa",
+    lastName: "Simpson",
+  },
+  {
+    firstName: "Maggie",
+    lastName: "Simpson",
+  },
+];
+
+export const testDonations = [
+  {
+    amount: "Cahir Castle",
+    method: "cash",
+    lat: "52.374500",
+    lng: "-7.927100",
+  },
+  {
+    amount: "Rock of Cashel",
+    method: "cash",
+    lat: "52.520100",
+    lng: "-7.890500",
+  },
+  {
+    amount: "Holy Cross Abbey",
+    method: "paypal",
+    lat: "52.381379",
+    lng: "-7.515999",
+  },
+];
